abe5f59b534dc862b83e6f97d2dcd8a43f326a5c
Guptamoheat <guptamohit801@gmail.com>
2024-03-16 15:39:09 +0530

    Package refactored and minor bugs fixed

--- spring-github-login-starter-master/src/main/java/io/javabrains/betterreads/userBooks/UserBooksRepository.java
+++
@@ -1,9 +0,0 @@
-package io.javabrains.betterreads.userBooks;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-
-public interface UserBooksRepository extends CassandraRepository<UserBooks, UserBookPrimaryKey>{
-    
-    
-}
